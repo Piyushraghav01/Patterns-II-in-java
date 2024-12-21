7a1f01721ab0151c66ed476e9e2f8ec9a2b53c09
Piyush Raghav <156657568+Piyushraghav01@users.noreply.github.com>
2024-12-21 18:10:57 +0530

    Add files via upload

---
+++ Pattern10.java
@@ -0,0 +1,124 @@
+
+// import java.util.Scanner;
+
+// public class Pattern {
+
+//     public static void main(String[] args) {
+//         int n;
+//         Scanner s = new Scanner(System.in);
+//         n = s.nextInt();
+//         int i = 1;
+//         while (i <= n) {
+//             int j = 1;
+//             while (j <= n) {
+//                 char jthChar = (char) ('A' + j - 1);
+//                 System.out.print(jthChar);
+//                 j = j + 1;
+//             }
+//             System.out.println();
+//             i = i + 1;
+//         }
+//     }
+// }
+
+// package patterns;
+// import java.util.Scanner;
+// public class Pattern9 {
+//     public static void main(String args[]) {
+//      Scanner s=new Scanner(System.in);
+//      int n=s.nextInt();
+//      int i=1;
+//      while(i<=n){
+//          char p=(char)('A'+i-1);
+//          int j=1;
+//          while(j<=n){
+             
+//              System.out.print(p);
+//              p=(char)(p+1);
+//              j=j+1;
+//          }
+//          System.out.println();
+//          i=i+1;
+//      }
+//     }
+// }
+
+
+// package patterns;
+// import java.util.Scanner;
+// public class Pattern10 {
+//     public static void main(String args[]) {
+//      Scanner s=new Scanner(System.in);
+//      int n=s.nextInt();
+//      int i=1;
+//      while(i<=n){
+//          int j=1;
+         
+//          while(j<=n-i+1){
+             
+//              System.out.print("*");
+//              j=j+1;
+//          }
+         
+//          System.out.println();
+//          i=i+1;
+//      }
+//     }
+// }
+
+// package patterns;
+// import java.util.Scanner;
+// public class Pattern11 {
+//     public static void main(String args[]) {
+//      Scanner s=new Scanner(System.in);
+//      int n=s.nextInt();
+//      int i=1;
+//      while(i<=n){
+//          int spaces=1;
+         
+//          while(spaces<=n-i){
+             
+//              System.out.print(" ");
+//              spaces=spaces+1;
+//          }
+//          int stars=1;
+//          while(stars<=i){
+//              System.out.print('*');
+//              stars=stars+1;
+//          }
+//          System.out.println();
+//          i=i+1;
+//      }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Pattern10 {
+//     public static void main(String args[]) {
+//      Scanner s=new Scanner(System.in);
+//      int n=s.nextInt();
+//      int i=1;
+//      while(i<=n){
+//          int spaces=1;
+         
+//          while(spaces<=n-i){
+             
+//              System.out.print(" ");
+//              spaces=spaces+1;
+//          }
+//          int num=1;
+//          while(num<=i){
+//              System.out.print(num);
+//              num=num+1;
+//          }
+//          int dec=i-1;
+//          while(dec>=1){
+//              System.out.print(dec);
+//              dec=dec-1;
+//          }
+//          System.out.println();
+//          i=i+1;
+//      }
+//     }
+// }
+
